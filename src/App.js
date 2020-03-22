9e24b63fe0639f45d657f5d855de8ebc2ba720d4
ofirelarat <96ofir11@gmail.com>
2020-03-22 21:47:23 +0200

    add skills tags

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import About from './components/about';
+import Skills from './components/skills';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <About />
+      <Header className="header-app" />
+      <div className="top-container">
+        <About />
+        <Skills />
+      </div>
     </div>
   );
 }
---
+++ src/components/skills/skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Chip } from '@material-ui/core';
+
+import './skills.css';
+
+function Skills(props) {
+    const skillsNames = ['Web Development', 'Android Development', 'Software Development',
+     '. Net Framework', '.Net Core', 'javascript', 'react', 'NodeJS', 'SQL', 'C#', 'JAVA'];
+
+     const chipsComp = skillsNames.map((skill) => <Chip className="skill" key={skill} label={skill}/>)
+    return (
+        <div className="skills">
+            <div className="title">Skills</div>
+            <div className="skills-container">
+                {chipsComp}
+            </div>
+        </div>
+    );
+  }
+  
+export default Skills;
