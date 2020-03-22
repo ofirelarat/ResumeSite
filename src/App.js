e066151d6247ef6fde37587637b4b601869b24ca
ofirelarat <96ofir11@gmail.com>
2020-03-22 12:57:34 +0200

    create header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>Welcome To Ofir Site!</p>
-      </header>
+      <Header />
     </div>
   );
 }
